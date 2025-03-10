bae598f303a2a0348ca171eb77e1a166b6956fd8
Miguel Dias <8230196@estg.ipp.pt>
2025-03-10 23:03:32 +0000

    Done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-
 // count initial ToDo
 countTodos();
+
+addEventListener("DOMContentLoaded", loadTasks())
+
 
 // capture click event
 document.getElementById('checkAll').addEventListener('click', function(){
@@ -30,7 +32,6 @@ for (var i = 0; i < todos.length; i++) {
     todos[i].addEventListener('change',function(){
         if(this.checked == true){
             var doneItem = this.parentElement.innerText
-            // $(this).parent().parent().parent().addClass('remove');
             console.log('done item: ' +doneItem);
             done(doneItem);
             countTodos();
@@ -43,37 +44,131 @@ var already_done_elements = document.getElementsByClassName("remove-item");
 for (var i = 0; i < already_done_elements.length; i++) {
     already_done_elements[i].addEventListener('click',function(){
         console.log(this);
-        removeItem(this);
+        removeItem(this.parentElement);
     });
 }
 
 // add new todo
 function addToDo(todo){
     createTodo(todo); 
+    saveTasks()
     countTodos();
 }
 
 // count tasks (To Complete)
 function countTodos(){
-  
+    let ulElem = document.getElementById("sortable")
+    let todos = ulElem.children.length;
+    
+    let itemsLeft = document.getElementsByClassName("count-todos")
+
+    itemsLeft[0].innerHTML = "Items Left: " + todos;
 }
 
 //create task (To Complete)
 function createTodo(text){
-    
+    let ulElem = document.getElementById("sortable")
+    let li = document.createElement("li");
+    li.className = "ui-state-default";
+
+    let div = document.createElement("div");
+    div.className = "checkbox";
+    let label = document.createElement("label");
+
+    let input = document.createElement("input");
+    input.type = "checkbox";
+    input.addEventListener('change',function(){
+        if(this.checked == true){
+            var doneItem = this.parentElement.innerText
+            console.log('done item: ' +doneItem);
+            done(doneItem);
+            li.remove();
+            countTodos();
+        }
+    });
+
+    label.appendChild(input);
+    label.appendChild(document.createTextNode(text));
+
+    div.appendChild(label);
+    li.appendChild(div);
+    ulElem.appendChild(li);
 }
 
 //mark task as done (To Complete)
 function done(doneItem){
-    
+    let ulElem = document.getElementById("done-items")
+    let li = document.createElement("li");
+    let button = document.createElement("button");
+    let span = document.createElement("span");
+    span.className = "fa fa-minus-square";
+    button.className = "remove-item btn btn-default btn-xs pull-right";
+    button.appendChild(span);
+    button.addEventListener('click',function(){
+        console.log(this);
+        removeItem(this.parentElement);
+    });
+    li.appendChild(document.createTextNode(doneItem));
+    li.appendChild(button);
+    ulElem.appendChild(li);
+
+    let todos = document.querySelectorAll("#sortable li");
+    for(let i = 0; i < todos.length; i++){
+        let label = todos[i].querySelector("label");
+        if(label && label.innerText.trim() === doneItem.trim()){ 
+            todos[i].remove();
+            break;
+        }
+    }
 }
 
 //mark all tasks as done (To Complete)
 function AllDone(){
-    
+    let ulElem = document.getElementById("sortable")
+    let todos = Array.from(ulElem.children);
+
+    for (let i = 0; i < todos.length; i++) {
+        let todo = todos[i];
+        let input = todo.querySelector("input");
+        input.checked = true;
+    }
+
+    for (let i = 0; i<todos.length; i++) {
+        done(todos[i].innerText);
+    }
+
+    for (let i =  todos.length-1;i>=0; i--) {
+        removeItem(todos[i]);
+    }
+
+    saveTasks()
+    countTodos();
 }
 
 //remove done task from list (To Complete)
 function removeItem(element){
-    
+    element.remove(); 
+}
+
+function saveTasks(){
+    let tasks = [];
+    let li = document.querySelectorAll('#sortable li');
+
+    for(let i = 0; i < li.length; i++){
+        tasks.push(li[i].innerText);
+    }
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function loadTasks(){
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    let ulElem = document.getElementById("sortable");
+    ulElem.innerHTML = "";
+
+    for(let i = 0; i < tasks.length; i++){
+        createTodo(tasks[i]);
+    }
+
+    countTodos();
 }
